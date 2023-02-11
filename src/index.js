299210bdeb719cdf3c2c96a0efe67b1f7657f720
Sablinita <sablinahare@gmail.com>
2023-02-12 00:46:03 +0100

    improved the footer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// These are the javascript codes for the hamburguer
 const hamburguerButton = document.getElementById('hamburguer')
 const navList = document.getElementById('nav-list')
 
@@ -5,4 +6,21 @@ function toggleButton(){
     navList.classList.toggle('show');
 }
 
-hamburguerButton.addEventListener('click', toggleButton)
+hamburguerButton.addEventListener('click', toggleButton);
+
+
+
+// These are the javascript codes for the footer and contact me form
+const $form = document.querySelector('#form')
+const $buttonMailto = document.querySelector('#trick')
+
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+  event.preventDefault()
+  const form = new FormData(this)
+  $buttonMailto.setAttribute('href', `mailto:sablinahare@gmail.com?subject=name ${form.get('name')}  email ${form.get('email')}&body=${form.get('message')}`)
+  $buttonMailto.click()
+};
+
+
